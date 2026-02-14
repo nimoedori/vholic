b98fb433dd099bb81b177d3a62b66f7feab2bea7
nimoes._. <118045257+bundangvholic@users.noreply.github.com>
2026-02-14 12:38:38 +0900

    [update] 2026-02-13

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let currentYear = new Date().getFullYear();
 /* ===== 게스트 데이터 (여기만 수정하면 됨) ===== */
 const guestData = {
   "2026-2-13": { male: "1/3", female: "2/3" },
-  "2026-2-15": { male: "0/3", female: "2/3" },
-  "2026-2-20": { male: "0/3", female: "0/3" },
+  "2026-2-15": { male: "1/3", female: "2/3" },
+  "2026-2-20": { male: "0/3", female: "1/3" },
   "2026-2-22": { male: "0/3", female: "0/3" },
   "2026-2-27": { male: "0/3", female: "0/3" },
   "2026-3-6": { male: "0/3", female: "1/3" }
